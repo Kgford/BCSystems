fac4c0360f8221253eb2256bf6d234a07ec4d5d5
Kgford <39444453+Kgford@users.noreply.github.com>
2020-08-03 11:52:07 -0400

    added If settings.DEBUG code

--- tcli/urls.py
+++ tcli/urls.py
@@ -23,3 +23,7 @@ urlpatterns = [
     path('equipment/', include("equipment.urls")),
     path('inventory/', include("inventory.urls")),
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
